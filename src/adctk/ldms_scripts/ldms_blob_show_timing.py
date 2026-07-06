7a0dc56ee9e3168400b12629dc4ab8e331503c6f
Ben Allan <baallan@sandia.gov>
2026-07-06 17:01:04 -0600

    fix security and other nits

--- src/adctk/ldms_scripts/ldms_blob_show_timing.py
+++ src/adctk/ldms_scripts/ldms_blob_show_timing.py
@@ -9,11 +9,13 @@
 #done
 import subprocess
 import sys
+import pathlib
 def main():
     files=sys.argv[1:]
     for i in files:
-        script = "od -A d -t u8 -j8 -w8 {}".format(i)
-        subprocess.call(script, shell=True)
+        if pathlib.Path(i).is_file():
+            script = "od -A d -t u8 -j8 -w8 {}".format(i)
+            subprocess.call(script, shell=True)
 
 if __name__ == '__main__':
     main()
